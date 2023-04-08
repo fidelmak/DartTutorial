e4fb437052170d722a3bd87b6b8ea1bac0899eca
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-04-08 09:50:58 +0100

    On branch master

---
+++ practise/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  var myString = "welcome to dart programming ";
+  print(myString);
+  // to make your code unsigned
+  final myString2 = "welcome to learning dart language";
+  print(myString2);
+  // we can also have something like this
+  String myString3;
+  myString3 = "fidelis";
+
+  // lets talk about null and non-nullable
+  String? impossible = null;
+  print(impossible?.length);
+}
