ea6a8e496a1dad41b9c3abdc150112e2d94cebf4
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-06-02 00:17:43 +0100

    Changes to be committed:

--- practise/main.dart
+++ practise/main.dart
@@ -1,15 +1,5 @@
+import 'scratch.dart';
+
 void main(List<String> args) {
-  List<int> aList = range['A']!;
-  int value1 = range['A']![0];
-  int value2 = range['A']![1];
-  double value = (value2 - value1) / 2;
-
-  print(aList);
-  print(value);
+  performTasks();
 }
-
-Map<String, List<int>> range = {
-  'A': [20, 29],
-  'B': [29, 30],
-  'C': [30, 39]
-};
---
+++ practise/scratch.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'dart:async';
+
+void main() {
+  performTasks();
+}
+
+Future<void> performTasks() async {
+  task1();
+  String task3Result = await task2();
+  task3(task3Result);
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future<String> task2() async {
+  Duration threeSeconds = const Duration(seconds: 3);
+  // instead of using sleep we can use future
+  //sleep(threeSeconds);
+  late String result;
+  await Future.delayed(threeSeconds, () {
+    result = 'task 2 data';
+    print('Task 2 complete');
+  });
+  return result;
+}
+
+void task3(String task2Data) {
+  String result = 'task 3 data';
+  print('Task 3 complete with $task2Data');
+}
