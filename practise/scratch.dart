c16d9b02cf1b2a7a93a99dcf8d66a011a679dd4c
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-06-02 21:36:17 +0100

    On branch master

--- practise/scratch.dart
+++ practise/scratch.dart
@@ -31,5 +31,5 @@ Future<String> task2() async {
 void task3(String task2Data) {
   String result = 'task 3 data';
   print('Task 3 complete with $task2Data');
-  print("where is this repo")
+  print("where is this repo");
 }
